a1fb224d9aa9bfca54777de9b3dc00db6700f369
Dhiaeddine1987 <dhiaeddineakoubi@hotmail.com>
2018-08-10 01:12:37 +0200

    create Adres and Person classes

---
+++ be.dhia.gitProject/src/main/java/be/dhia/gitProject/entities/Adres.java
@@ -0,0 +1,5 @@
+package be.dhia.gitProject.entities;
+
+public class Adres {
+
+}
---
+++ be.dhia.gitProject/src/main/java/be/dhia/gitProject/entities/Person.java
@@ -0,0 +1,72 @@
+package be.dhia.gitProject.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Person implements Serializable{
+	
+	private long id;
+	private String firstName;
+	private String lastName;
+	private Date birthdate;
+	private Adres adress;
+	
+	
+	public Person() {
+		super();
+	}
+
+	public Person(long id, String firstName, String lastName, Date birthdate, Adres adress) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthdate = birthdate;
+		this.adress = adress;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public Adres getAdress() {
+		return adress;
+	}
+
+	public void setAdress(Adres adress) {
+		this.adress = adress;
+	}
+	
+	
+	
+	
+
+}
